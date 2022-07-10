c60bcb45e39f509a80328a9b007f74a5810c4941
octavia <Ogomes21@gmail.com>
2022-07-10 18:05:04 -0400

    Finished everything.

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -3,33 +3,72 @@ package com.zipcodewilmington.person;
 /**
  * Created by leon on 2/12/18.
  */
+//public class Person {
+//    private String name;
+//    private int age;
+//
+//    public Person() {
+//    }
+//
+//    public Person(int age) {
+//    }
+//
+//    public Person(String name) {
+//    }
+//
+//    public Person(String name, int age) {
+//    }
+//
+//    public void setName(String name) {
+//    }
+//
+//    public void setAge(int age) {
+//    }
+//
+//    public String getName() {
+//        return null;
+//    }
+//
+//    public Integer getAge() {
+//        return null;
+//    }
+//}
+
 public class Person {
     private String name;
     private int age;
 
     public Person() {
+        name = "";
+        age = Integer.MAX_VALUE;
     }
 
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public Integer getAge() {
-        return null;
+        return this.age;
     }
 }
